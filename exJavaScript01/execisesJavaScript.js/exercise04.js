950dcef5f6f7bf960d778a3ceebf91ac6ea7d211
sofia-wildner <sofiawildner11@gmail.com>
2026-07-16 10:30:48 -0300

    Add files via upload

---
+++ exJavaScript01/execisesJavaScript.js/exercise04.js
@@ -0,0 +1,16 @@
+let age = "22"
+console.log(typeof age)
+
+let πρ = "2850.50"
+console.log(typeof πρ)
+
+let withChildren = "1"
+console.log(typeof withChildren)
+
+Number(age)
+Number(πρ)
+Number(withChildren)
+
+console.log(typeof age)
+console.log(typeof πρ)
+console.log(typeof withChildren)
---
+++ exJavaScript01/execisesJavaScript.js/exercise05.js
@@ -0,0 +1,12 @@
+let keyboard = 249.90
+let mouse = 119.90
+let headset = 349.90
+
+let result = (keyboard + mouse + headset)
+console.log(result) //719.7
+
+let resultDivision = (result / 3)
+console.log (resultDivision) //239.9
+
+let change = (1000 % result)
+console.log (change) //280.29999999999995
